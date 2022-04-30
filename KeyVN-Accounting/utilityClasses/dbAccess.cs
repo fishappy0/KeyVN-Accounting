f3c660399f94a797ce752aaed19c155eb2b54c18
Fishappy0 <fishappy0@gmail.com>
2022-05-01 00:28:00 +0700

    Adding utility classes

--- KeyVN-Accounting/utilityClasses/dbAccess.cs
+++ KeyVN-Accounting/utilityClasses/dbAccess.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
 
 namespace KeyVN_Accounting
 {
     internal class dbAccess
     {
+        private SqlConnection conn = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["connString"].ConnectionString)
+        );
+        private String QUERY = "";
+
+        public dbAccess(String QUERY = "SELECT * FROM Employee")
+        {
+            this.QUERY = QUERY;
+            try
+            {
+                this.conn.Open();
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
+
+        public void setQuery(String QUERY)
+        {
+            this.QUERY = QUERY;
+        }
+
+        public System.Data.DataTable getDT(String QUERY = "SELECT * FROM Employee")
+        {
+            this.QUERY = QUERY;
+            System.Data.DataTable DT = new System.Data.DataTable();
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open) conn.Open();
+                System.Data.SqlClient.SqlCommand login = new SqlCommand(QUERY, this.conn);
+                System.Data.SqlClient.SqlDataAdapter DA = new SqlDataAdapter(login);
+                DA.Fill(DT);
+                conn.Close();
+                return DT;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+                return DT;
+            }
+        }
+
     }
 }
--- KeyVN-Accounting/utilityClasses/wordProcessing.cs
+++ KeyVN-Accounting/utilityClasses/wordProcessing.cs
@@ -3,11 +3,151 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+using System.IO;
 
 namespace KeyVN_Accounting
 {
     internal class wordProcessing
     {
+        private Word.Application wordApp;
+        private Word.Document wordDoc;
+        private String fileLocation;
 
+        public wordProcessing()
+        {
+            wordApp = new Word.Application();
+        }
+
+        public void setWordDocWithAddress(String fileAddress)
+        {
+            try
+            {
+                wordDoc = wordApp.Documents.Open(fileAddress);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        public void closeInstance()
+        {
+            wordDoc.Close(SaveChanges: false);
+            wordDoc = null;
+            wordApp.Quit(SaveChanges: false);
+        }
+
+        public void saveInstance(string Address)
+        {
+            wordDoc.SaveAs2(Address);
+        }
+
+        private void findAndReplaceWith(Word.Application wordApp, object findText, object replaceWithText)
+        {
+            wordApp.Selection.Find.Execute(ref findText, true,
+                                true, false, false, false, false, 1, false,
+                                 replaceWithText, 2, false, false, false, false);
+        }
+
+        public void printWordDoc()
+        {
+            try
+            {
+                PrintDialog pDiag = new PrintDialog();
+                if (pDiag.ShowDialog() == DialogResult.OK)
+                {
+                    this.wordApp.ActivePrinter = pDiag.PrinterSettings.PrinterName;
+                    this.wordDoc.PrintOut();
+                    // This sleep timer is to wait for ms to send the print command
+                    // to the printer and ms doesn't have any method to wait for this
+                    System.Threading.Thread.Sleep(4000);
+                    this.wordApp.ActivePrinter = null;
+                    this.closeInstance();
+                    MessageBox.Show("Sent to Printer");
+                }
+                else
+                {
+                    this.closeInstance();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Lỗi" + error.Message);
+                this.closeInstance();
+            }
+        }
+
+        public void printFromAddress(String fileAddress)
+        {
+            try
+            {
+                this.fileLocation = fileAddress;
+                this.wordDoc = wordApp.Documents.Open(fileLocation);
+                this.printWordDoc();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+                this.closeInstance();
+            }
+        }
+
+        public void replaceWords(Dictionary<String, String> find_replace_list)
+        {
+            try
+            {
+                foreach (var word in find_replace_list)
+                {
+                    String findWord = word.Key;
+                    String replaceWord = word.Value;
+                    this.findAndReplaceWith(wordApp, findWord, replaceWord);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
+
+        //check the table layout then edit this again
+        public void customFillTable(DataGridView Grid)
+        {
+            try
+            {
+                int colIndex = 0;
+                int rowIndex = 0;
+                Word.Table Table = wordDoc.Tables[1];
+                Grid.AllowUserToAddRows = false;
+
+                foreach (DataGridViewRow row in Grid.Rows)
+                {
+                    if (rowIndex < Grid.Rows.Count - 1)
+                    {
+                        Table.Rows.Add(Table.Cell(1, 1));
+                    }
+                    colIndex = 0;
+                    foreach (DataGridViewColumn col in Grid.Columns)
+                    {
+                        var gridCellValue = Grid[colIndex, rowIndex].Value;
+                        if (gridCellValue != null)
+                        {
+                            Table.Cell(rowIndex + 1, colIndex).Range.Text = gridCellValue.ToString();
+                        }
+                        else
+                        {
+                            Table.Cell(rowIndex + 1, colIndex).Range.Text = "";
+                        }
+                        colIndex++;
+                    }
+                    rowIndex++;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+        }
     }
 }
